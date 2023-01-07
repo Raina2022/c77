6dda2710c9806594616adbca9be04b42bc0c4cb4
Raina2022 <110728855+Raina2022@users.noreply.github.com>
2023-01-07 09:08:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name="Raina";
+var last_name="Agarwal";
+var fullname=first_name.concat(last_name);
+console.log(fullname);
+var a=3;
+var b=2;
+var add=a+b;
+console.log(add);
+var sub=a-b;
+console.log (sub);
